561c0b3ec91c4ba6120ccfd9704c3d19651e8754
JohnnyClark <Jonathanclark553@gmail.com>
2024-06-23 22:48:35 -0500

    Test commit for Jira , G2FC-118

---
+++ flex_fit/src/pages/testPage.js
@@ -0,0 +1,28 @@
+//testPage.js
+
+import '../styles/testPage.css';
+
+function TestPage() {
+
+    return (
+        <div className="main-background"> 
+        <h1> Thy code is here.</h1>
+        <h2> second header. </h2>
+        <div>
+
+        <button>
+
+            Hello World!
+
+        </button>
+
+        </div>
+
+        </div>
+
+
+    );
+
+}
+
+export default TestPage;
